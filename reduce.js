8dafc27caaf3238baa6adeb212f987fb0e3816d3
marinamarshall <marinamarshall2@gmail.com>
2021-11-07 14:47:50 +0000

    Add reduce methods

--- reduce.js
+++ reduce.js
@@ -7,6 +7,10 @@
 
 // Summing an array of numbers:
 
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
+
 
 const teamMembers = [
   {
@@ -31,7 +35,25 @@ const teamMembers = [
   }
 ];
 
-// Totaling a specific object property
+// Totaling a specific object property,
+// totaling the values of a specific property in  an array of objects,
+
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 
 // Grouping by a property, and totaling it too
+// Restructuring data in different ways
+// {Developer: 12, Designer: 4} <---- This is what we want
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
